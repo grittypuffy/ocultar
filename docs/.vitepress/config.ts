bc6e88d14fc6b21fcddf7e7a6ebb2daeaaf8da92
grittypuffy <grittypuffy@riseup.net>
2024-10-16 17:55:12 +0530

    "Update: Include icons for Tauri desktop application"

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Ocultar Docs",
+  description: "Documentation for Ocultar"
+})
