7786775d2c0b09118d04da63c61f954064e42db9
grittypuffy <grittypuffy@riseup.net>
2024-10-19 22:07:20 +0530

    Add: Language detection and OCR for images and PDF for dynamic language detection

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,21 +10,25 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import uvicorn
 
+import pytesseract
+import cv2
+from PIL import Image
 from lingua import Language, LanguageDetectorBuilder
 
 try:
     import spacy
     import en_core_web_lg
-    import es_core_news_lg 
 except Exception as e:
-    print(e.args)
+    match e.args:
+        case ("No module named 'en_core_web_lg'",):
+            spacy.cli.download("en_core_web_lg")
+try:
+    import es_core_news_lg
+except Exception as e:
     match e.args:
         case ("No module named 'es_core_news_lg'",):
             spacy.cli.download("es_core_news_lg")
-        case ("No module named 'en_core_web_lg'",):
-            spacy.cli.download("en_core_web_lg")
-    import en_core_web_lg
-    import es_core_news_lg
+
 
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
@@ -33,31 +37,29 @@ from presidio_image_redactor import ImageAnalyzerEngine
 
 from pdf2image import convert_from_bytes
 import img2pdf
-from PIL import Image
 
 from pathlib import Path
 
-image_analyzer_engine = ImageAnalyzerEngine()
-image_redactor_engine = ImageRedactorEngine()
-analyzer = AnalyzerEngine()
+analyzer = AnalyzerEngine(supported_languages=["en", "es"])
 anonymizer = AnonymizerEngine()
+
+image_analyzer_engine = ImageAnalyzerEngine(analyzer_engine=analyzer)
+image_redactor_engine = ImageRedactorEngine()
 
 app = FastAPI()
 
-def _setup_middleware():
-    origins = [
-        "http://0.0.0.0:3000/",
-    ]
+origins = [
+    "http://0.0.0.0:3000/",
+]
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-_setup_middleware()
 
 def _setup_lang_detection():
     languages = [Language.ENGLISH, Language.SPANISH]
@@ -70,20 +72,28 @@ match os.path.sys.platform:
         STORAGE_PATH = Path("/var/tmp/ocultar/").absolute()
     case 'windows':
         STORAGE_PATH = Path("C:\\Temp\\Ocultar\\").absolute()
+    case _:
+        import sys
+        print("Unsupported platform")
+        sys.exit(127)
+
 try:
-    os.mkdir("STORAGE_PATH")
+    os.mkdir(STORAGE_PATH)
 except FileExistsError:
     pass
 
 detector = _setup_lang_detection()
 
+import time
+
 class RedactFile:
     def get_redacted_filename(filename: str, extension: str | None, or_extension: str) -> str:
-        redacted_file_name = os.path.join(STORAGE, filename + ".redacted." + (extension or or_extension))
+        redacted_file_name = filename + ".redacted." + (extension or or_extension)
+        return redacted_file_name
 
     def get_filename_hash(filename: str) -> str:
         hash_object = hashlib.sha256()
-        hash_object.update(filename.encode("utf-8"))
+        hash_object.update(f"{filename}{time.time_ns()}".encode("utf-8"))
         return hash_object.hexdigest()
 
     def anonymize_text(file: UploadFile):
@@ -99,18 +109,47 @@ class RedactFile:
         language_code = language.iso_code_639_1.name.lower()
         return language_code if language_code in SUPPORTED_LANGUAGES else None
 
-    def write_text(anonymized_text: str, redacted_file_name: str) -> str:
-        with open(redacted_file_name, "w") as redacted_text_file:
+    def write_text(anonymized_text: str, redacted_file_path: str) -> str:
+        with open(redacted_file_path, "w") as redacted_text_file:
             redacted_text_file.write(anonymized_results.text)
 
-    def redact_image(file: UploadFile) -> str:
+    def redact_image(file: UploadFile, redacted_file_path: str) -> str:
         content = io.BytesIO(file.file.read())
-        image = Image.open(content)
-        redacted_image = image_redactor_engine.redact(image, (0, 0, 0))
-        redacted_image.save(redacted_file_name)
+        original_image = Image.open(content)
+        print(redacted_file_path)
+        original_image.save(redacted_file_path)
+        preprocessed_image = cv2.imread(redacted_file_path)
+        #convert to grayscale image
+        gray=cv2.cvtColor(preprocessed_image, cv2.COLOR_BGR2GRAY)
+        
+        # Threshold and blurring for pre-processing. Needed for accuracy in text detection.
+        cv2.threshold(gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
+        cv2.medianBlur(gray, 3)
+        cv2.imwrite(redacted_file_path, gray)
+
+        text = pytesseract.image_to_string(Image.open(redacted_file_path))
+        language = RedactFile.detect_text_language(text=text)
+        os.remove(redacted_file_path)
+
+        redacted_image = image_redactor_engine.redact(original_image, (0, 0, 0), language=language)
+        redacted_image.save(Path(redacted_file_path).absolute())
 
     def send_file(original_file_name: str, redacted_file_path: str, media_type: str) -> FileResponse:
         return FileResponse(redacted_file_path, media_type="text/plain", filename=original_file_name)
+    """
+    def return_extension(content_type: str) -> str:
+        match content_type:
+            case "image/jpeg":
+                return "jpg"
+            case "image/png":
+                return "png"
+            case "image/gif":
+                return "gif"
+            case "application/pdf":
+                return "pdf"
+            case "text/plain":
+                return "txt"
+    """
 
 SUPPORTED_EXTENSIONS = ("jpeg", "jpg", "png", "gif", "pdf", "txt")
 SUPPORTED_LANGUAGES = ("en", "es")
@@ -118,44 +157,62 @@ SUPPORTED_LANGUAGES = ("en", "es")
 @app.post("/upload")
 async def upload(file: UploadFile) -> FileResponse:
     file_name_split = file.filename.split(".")
-    original_filename = "".join(file_name_split(".")[:-1])
-    extension = file_name_split[-1] if file_name_split[-1] in ALLOWED_EXTENSIONS else None
+    original_filename = "".join(file_name_split[:-1])
+    extension = file_name_split[-1] if file_name_split[-1] in SUPPORTED_EXTENSIONS else None
     mime = file.content_type
     redacted_file_name_hash = RedactFile.get_filename_hash(original_filename)
-    redacted_file_name = RedactFile.get_redacted_filename(redacted_file_name_hash, extension=extension, or_extension="txt")
-    redacted_file_path = Path(STORAGE_PATH + redacted_file_name).absolute()
-
+    redacted_file_name = RedactFile.get_redacted_filename(redacted_file_name_hash, extension=extension, or_extension=file.content_type.split("/")[-1])
+    redacted_file_path = Path(os.path.join(STORAGE_PATH, redacted_file_name)).absolute()
     match file.content_type:
         case "text/plain":
             anonymized_text = RedactFile.anonymize_text(file)
-            RedactFile.write_text(anonymized_results.text, redacted_file_name)
-            return RedactFile.send_file(original_filename, redacted_file_name, media_type=file.content_type)
+            RedactFile.write_text(anonymized_results.text, redacted_file_path)
+            return RedactFile.send_file(original_filename, redacted_file_path, media_type=file.content_type)
 
-        case "image/jpeg" | "image/png" | "image/gif":
-            redact_image(file)
-            return RedactFile.send_file(original_filename, redacted_file_name, media_type=file.content_type)
+        case "image/jpeg" | "image/png" | "image/gif" | "image/svg+xml":
+            RedactFile.redact_image(file, redacted_file_path)
+            return RedactFile.send_file(original_filename, redacted_file_path, media_type=file.content_type)
 
         case "application/pdf":
-            hashed_directory = Path(STORAGE + redacted_file_name_hash).absolute()
+            hashed_directory = Path(os.path.join(STORAGE_PATH, redacted_file_name_hash)).absolute()
             try:
                 os.mkdir(hashed_directory)
             except:
                 shutil.rmtree(hashed_directory)
                 os.mkdir(hashed_directory)
+
             images = convert_from_bytes(pdf_file=file.file.read(), output_folder=hashed_directory)
             for (index, image) in enumerate(images):
-                image_redactor_engine.redact(image, (0, 0, 0)).convert("RGB").save(Path(hashed_directory + f"{filename}-{index}.jpg"), "JPEG")
-            for file in os.listdir(hashed_directory):
-                if file.endswith(".ppm"):
-                    os.remove(Path(hashed_directory + file).absolute())
+                image_path = Path(os.path.join(hashed_directory, f"{redacted_file_name_hash}-{index}.jpg"))
+                original_image = Image.open(image_path)
+                original_image.save(image_path)
+                preprocessed_image = cv2.imread(image_path)
+                
+                #convert to grayscale image
+                gray=cv2.cvtColor(preprocessed_image, cv2.COLOR_BGR2GRAY)
+                
+                # Threshold and blurring for pre-processing. Needed for accuracy in text detection.
+                cv2.threshold(gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
+                cv2.medianBlur(gray, 3)
+                cv2.imwrite(image_path, gray)
 
-            images = [Path(hashed_directory + file).absolute() for image in os.listdir(hashed_directory)]
-            with open(redacted_file_name, "wb") as redacted_file:
+                text = pytesseract.image_to_string(Image.open(image_path))
+                language = RedactFile.detect_text_language(text=text)
+                os.remove(redacted_file_path)
+
+                image_redactor_engine.redact(original_image, (0, 0, 0), language=language).convert("RGB").save((image_path).absolute(), "JPEG")
+
+            for intermediate_file in os.listdir(hashed_directory):
+                if intermediate_file.endswith(".ppm"):
+                    os.remove(Path(os.path.join(hashed_directory, intermediate_file)).absolute())
+            images = [Path(os.path.join(hashed_directory, image)).absolute() for image in os.listdir(hashed_directory)]
+
+            with open(redacted_file_path, "wb") as redacted_file:
                 redacted_file.write(img2pdf.convert(images))
+
             shutil.rmtree(hashed_directory)
-            return RedactFile.send_file(original_filename, redacted_file_name, media_type=file.content_type)
+            return RedactFile.send_file(original_filename, redacted_file_path, media_type=file.content_type)
 
 
 if __name__ == "__main__":
-    print("API working")
     uvicorn.run(app, host="0.0.0.0", port=8000)
